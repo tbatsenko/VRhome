36b15b59cce2c5f0d56cb936d86cada822fb178f
Tanya <tbatsenko@gmail.com>
2017-05-14 02:51:30 +0300

    display speech -4

--- static/js/set-image.js
+++ static/js/set-image.js
@@ -151,12 +151,12 @@ var start_timestamp;
         console.log(interim_transcript);
         //"<a-text id="mytext" value="Say: Hello World! to start" position="" rotation="" scale="" visible="" text=""></a-text>"
 
-        document.querySelector('#mytext').outerHTML='<a-text id="mytext" value="${interim_transcript}" position="" rotation="" scale="" visible="" text=""></a-text>';
+        document.querySelector('#mytext').outerHTML='<a-text id="mytext" value="{{interim_transcript}}" position="" rotation="" scale="" visible="" text=""></a-text>';
         if (interim_transcript =="stop" || interim_transcript=="Stop" || interim_transcript ==" stop" ||interim_transcript ==" Stop") {
             recognition.stop();
             var sceneEl = document.querySelector('a-scene');
             console.log(sceneEl.querySelector('#mytext').value);
-            sceneEl.querySelector('#mytext').outerHTML='<a-text id="mytext" value="${interim_transcript}" position="" rotation="" scale="" visible="" text=""></a-text>';
+            sceneEl.querySelector('#mytext').outerHTML='<a-text id="mytext" value="{{interim_transcript}}" position="" rotation="" scale="" visible="" text=""></a-text>';
             console.log("STOPPED");
 
         }
