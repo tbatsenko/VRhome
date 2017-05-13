05234e04a2cef6582cbd14f60d0a24f97d500f7b
Tanya <tbatsenko@gmail.com>
2017-05-14 01:32:16 +0300

    add voice recognition to A-frame -2 one word rec

--- static/js/set-image.js
+++ static/js/set-image.js
@@ -119,9 +119,9 @@ var start_timestamp;
     var interim_transcript = '';
     for (var i = event.resultIndex; i < event.results.length; ++i) {
       if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
+        final_transcript = event.results[i][0].transcript;
       } else {
-        interim_transcript += event.results[i][0].transcript;
+        interim_transcript = event.results[i][0].transcript;
       }
     }
     final_transcript = capitalize(final_transcript);
