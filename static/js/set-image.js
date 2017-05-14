8413b0ad05db460132b938811720129728accdc5
Tanya <tbatsenko@gmail.com>
2017-05-14 14:27:40 +0300

    manage cursor

--- static/js/set-image.js
+++ static/js/set-image.js
@@ -198,3 +198,15 @@ var first_char = /\S/;
 function capitalize(s) {
   return s.replace(first_char, function(m) { return m.toUpperCase(); });
 }
+
+
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    var COLORS = ['red', 'green', 'blue'];
+    this.el.addEventListener('click', function (evt) {
+      var randomIndex = Math.floor(Math.random() * COLORS.length);
+      this.setAttribute('material', 'color', COLORS[randomIndex]);
+      console.log('I was clicked at: ', evt.detail.intersection.point);
+    });
+  }
+});
