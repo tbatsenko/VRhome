2ca1a270f87ae5fb3079b5cd081795eacc40c4b4
Tanya <tbatsenko@gmail.com>
2017-05-14 14:30:45 +0300

    manage cursor

--- static/js/set-image.js
+++ static/js/set-image.js
@@ -205,8 +205,9 @@ AFRAME.registerComponent('cursor-listener', {
   init: function () {
     var COLORS = ['red', 'green', 'blue'];
     this.el.addEventListener('click', function (evt) {
-      var randomIndex = Math.floor(Math.random() * COLORS.length);
-      this.setAttribute('material', 'color', COLORS[randomIndex]);
+    //   var randomIndex = Math.floor(Math.random() * COLORS.length);
+    //   this.setAttribute('material', 'color', COLORS[randomIndex]);
+      activeBox = this;
       console.log('I was clicked at: ', evt.detail.intersection.point);
     });
   }
